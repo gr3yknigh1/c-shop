aaf2ee559660c813119f7b2d423cc51461122849
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-10-05 14:21:01 +0300

    Make basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import styles from "@styles/Index.module.css";
+import Head from "next/head";
 import type { NextPage } from "next";
 
 const Index: NextPage = () => {
   return (
-    <div>
-      Hi hi there!
+    <div className={styles.container}>
+      <Head>
+        <title>C Shop</title>
+        <meta name="description" content="Awesome cloth shop" />;
+      </Head>
+      <header>
+        <h1>C Shop</h1>
+        <nav></nav>
+      </header>
+      <main>
+        <section>
+          <div>Hi hi there</div>
+        </section>
+      </main>
+      <footer>
+        <address>
+          <a href="mailto:feedback@cshop.com">feedback@cshop.com</a>
+          <a href="tel:+88888888888">+8 (888) 888 88 88</a>
+        </address>
+      </footer>
     </div>
   );
 };
