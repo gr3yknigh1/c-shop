775527e475221a2ae5ed458b336b87b58f32bd48
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-10-09 14:51:47 +0300

    Remove unecessary code

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,21 +12,10 @@ const Index: NextPage = () => {
         <meta name="description" content="Awesome cloth shop" />;
       </Head>
       <header>
-        <h1 className={styles.header}>C Shop</h1>
-        <nav></nav>
       </header>
       <main>
-        <section>
-          <div>Hi hi there</div>
-          <PlaceholderImage width={100} height={100} />
-        </section>
       </main>
-      <footer className={styles.footer}>
-        <address>
-          <a href="mailto:feedback@cshop.com">feedback@cshop.com</a>
-          <br />
-          <a href="tel:+88888888888">+8 (888) 888 88 88</a>
-        </address>
+      <footer>
       </footer>
     </div>
   );
